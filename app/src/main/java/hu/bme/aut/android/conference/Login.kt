74723fd5297ccc3b981ba21a012de7ab30e93a48
Suszter Dominik <suszterdominik@gmail.com>
2020-10-24 23:27:43 +0200

    Bejelentkező felület

---
+++ app/src/main/java/hu/bme/aut/android/conference/Login.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.android.conference
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+public class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
