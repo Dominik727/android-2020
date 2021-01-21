f70b3505809d3b6a69b212b4921f102e74119947
Suszter Dominik <suszterdominik@gmail.com>
2021-01-21 20:01:40 +0100

    Huwaei Push Kit add

---
+++ app/src/main/java/hu/bme/aut/android/conference/PushActivity.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021
+ * Created by Suszter Dominik on 2021. 1. 21
+ * Copyright © 2020. RR. All rights reserved.
+ */
+
+package hu.bme.aut.android.conference
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import hu.bme.aut.android.conference.Base.BaseActivity
+
+class PushActivity: BaseActivity() {
+}
